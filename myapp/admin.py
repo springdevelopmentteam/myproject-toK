5b07b10e1a5cc01e361a80d2cc3126e3c7ef97b1
springdevelopmentteam <107068134+springdevelopmentteam@users.noreply.github.com>
2022-06-16 18:31:41 +0630

    Update admin.py

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 from .models import *
 
+class ArtistAdmin(admin.ModelAdmin):
+    list_display = ['artist_name', 'nickname', 'artist_image','email','created_at','bio']
+    list_filter = ['artist_name', 'nickname']
+
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ['album_name','artist','year_released']
+    list_filter = ['album_name','artist','year_released']
+
+class SongAdmin(admin.ModelAdmin):
+    list_display = ['song_name','album','writer','artist','song_image','duration']
+    list_filter = ['song_name','album','writer','artist']
+
 admin.site.register(Artist)
 admin.site.register(Album)
 admin.site.register(Song)
